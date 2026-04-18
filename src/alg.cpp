1ae8f1dde273a617af0e1be92647cb4226663c39
EkaterinaShkolnikk <75201487+EkaterinaShkolnikk@users.noreply.github.com>
2026-04-18 16:17:52 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,93 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(const std::string& inf) {
-  // добавьте код
-  return std::string("");
+int OperPriority(char sym) {
+    if (sym == '(')
+        return 0;
+    else if (sym == ')')
+        return 1;
+    else if (sym == '+' || sym == '-')
+        return 2;
+    else
+        return 3;
 }
 
-int eval(const std::string& pref) {
-  // добавьте код
-  return 0;
+bool Type(char sym) {
+    if (sym >= '0' && sym <= '9')
+        return true;
+    return false;
 }
+
+
+std::string infx2pstfx(const std::string& inf) {
+    TStack<char, 100> stack;
+    std::string postfix;
+
+    for (char sym : inf) {
+        if (Type(sym)) {
+            postfix.push_back(sym);
+            postfix.push_back(' ');
+        } else if (OperPriority(sym) == 0) {
+            stack.push(sym);
+        } else if (OperPriority(sym) == 1) {
+            while (!stack.isEmpty() && OperPriority(stack.get()) != 0) {
+                postfix.push_back(stack.get());
+                postfix.push_back(' ');
+                stack.pop();
+            }
+            stack.pop();
+        } else {
+            while (!stack.isEmpty() && (OperPriority(sym) <= OperPriority(stack.get()))) {
+                postfix.push_back(stack.get());
+                postfix.push_back(' ');
+                stack.pop();
+            }
+            stack.push(sym);
+        }
+    }
+
+    while (!stack.isEmpty()) {
+        postfix.push_back(stack.get());
+        postfix.push_back(' ');
+        stack.pop();
+    }
+
+    if (!postfix.empty()) {
+        postfix.pop_back();
+    }
+
+    return postfix;
+}
+
+
+   int eval(const std::string& post) {
+       TStack<int, 100> stack;
+
+       for (char sym : post) {
+           if (Type(sym)) {
+               stack.push(sym - '0');
+           } else if (sym == '+' || sym == '-' || sym == '/' || sym == '*') {
+               int numberTwo = stack.get();
+               stack.pop();
+               int numberOne = stack.get();
+               stack.pop();
+
+               switch (sym) {
+                   case '+':
+                       stack.push(numberOne + numberTwo);
+                       break;
+                   case '-':
+                       stack.push(numberOne - numberTwo);
+                       break;
+                   case '*':
+                       stack.push(numberOne * numberTwo);
+                       break;
+                   case '/':
+                       stack.push(numberOne / numberTwo);
+                       break;
+               }
+           }
+       }
+
+       return stack.get();
+   }
